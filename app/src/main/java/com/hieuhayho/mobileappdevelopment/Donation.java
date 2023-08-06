e223c7137a95a148e1a0d7b6c714a439c8a3ef98
Hiếu Hay Ho <truongxuanhieu11@gmail.com>
2023-08-06 14:03:43 +0700

    Initial commit

---
+++ app/src/main/java/com/hieuhayho/mobileappdevelopment/Donation.java
@@ -0,0 +1,28 @@
+package com.hieuhayho.mobileappdevelopment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.NumberPicker;
+import android.widget.ProgressBar;
+
+public class Donation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donation);
+
+        NumberPicker numberPicker = findViewById(R.id.numberPicker);
+        numberPicker.setMinValue(1);
+        numberPicker.setMaxValue(2000);
+        numberPicker.setValue(1000);
+
+        numberPicker.setOnValueChangedListener((picker, oldVal, newVal) -> {
+            // Handle value change here
+        });
+
+        ProgressBar progressBar = findViewById(R.id.progressBar);
+        progressBar.setProgress(20);
+    }
+}
---
+++ app/src/main/java/com/hieuhayho/mobileappdevelopment/EssentialsOfDevelopingAndroid.java
@@ -0,0 +1,14 @@
+package com.hieuhayho.mobileappdevelopment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EssentialsOfDevelopingAndroid extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_essentials_of_developing_android);
+    }
+}
